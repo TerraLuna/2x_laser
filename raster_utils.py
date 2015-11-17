4ef5bc8b6c107fe39bd0002c725139394bfe0d7a
Paul Kastner <misc@paulkastner.com>
2015-11-17 12:13:47 -0800

    added indentation (not sure how it went away)

--- raster_utils.py
+++ raster_utils.py
@@ -8,17 +8,17 @@ emc_ini = None
 def open_raster_fifo(mode):
     global emc_ini
     if emc_ini is None:
-	emc_ini = linuxcnc.ini('2x_laser.ini')
+        emc_ini = linuxcnc.ini('2x_laser.ini')
     pipefile = emc_ini.find('RASTER', 'PIPE_FILE')
 
     if pipefile is None:
-	pipefile = '/tmp/emc2_raster_fifo';
+        pipefile = '/tmp/emc2_raster_fifo';
 
     try:
-	return open(pipefile, mode)
+        return open(pipefile, mode)
     except IOError:
-	os.mkfifo(pipefile)
-	return open(pipefile, mode)
+        os.mkfifo(pipefile)
+    return open(pipefile, mode)
 
 def send_params(p, q):
     fp = open_raster_fifo('w')
@@ -36,9 +36,9 @@ def get_comment(file, lineno):
     fp = open(file, 'r')
     lineno = lineno - 1
     for i, line in enumerate(fp):
-	if lineno == i:
-	    break
+        if lineno == i:
+            break
     m = re.search('\(([^)]+)\)', line)
     if m is not None:
-	return m.group(1).strip()
+        return m.group(1).strip()
     return ''
